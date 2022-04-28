f68f72e02abad650a866331c63cfdfa62008a134
Joel <joel@inka-labs.com>
2022-04-28 16:29:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
+import ReactSpeedometer from 'react-d3-speedometer';
+import { useForm } from 'react-hook-form';
+
+const baseURL = 'http://localhost:8000';
+
+const getRatio = async (params) => {
+	const newRatio = await axios.get(`${baseURL}/assesments/`, { params });
+	return newRatio.data;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { register, handleSubmit, watch } = useForm();
+	const [ratio, setRatio] = useState(0);
+
+	const onSubmit = useCallback(
+		async (data) => {
+			try {
+				const { ratio: newRatio } = await getRatio(data);
+				setRatio(newRatio);
+			} catch (error) {
+				console.error(error);
+			}
+		},
+		[getRatio]
+	);
+
+	return (
+		<div className="App">
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input defaultValue="Account Manager" {...register('jobTitle')} />
+				<input defaultValue="New York" {...register('location')} />
+				<input type="submit" />
+			</form>
+			<ReactSpeedometer minValue={0.001} maxValue={0.5} value={ratio} />
+		</div>
+	);
 }
 
 export default App;
